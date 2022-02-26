1836264bd4ffbd79ad93ad4a81d2970681f6335d
Alistair Smith <hi@alistair.sh>
2022-02-26 13:16:46 +0000

    h

--- apps/loc-processor/src/constants.ts
+++ apps/loc-processor/src/constants.ts
@@ -1,9 +1,8 @@
 import {envsafe, str} from 'envsafe';
 
-export const {AWS_REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, SQS_URL} =
-	envsafe({
-		AWS_REGION: str(),
-		AWS_ACCESS_KEY_ID: str(),
-		AWS_SECRET_ACCESS_KEY: str(),
-		SQS_URL: str(),
-	});
+export const env = envsafe({
+	AWS_REGION: str(),
+	AWS_ACCESS_KEY_ID: str(),
+	AWS_SECRET_ACCESS_KEY: str(),
+	SQS_URL: str(),
+});
