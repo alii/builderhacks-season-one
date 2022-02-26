7c675efc8ca8dc7bc50d8a980053fd041ff01345
Alistair Smith <hi@alistair.sh>
2022-02-26 21:33:09 +0000

    rh

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,11 +1,10 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import {AppProps} from 'next/app';
 import {SWRConfig} from 'swr';
 import {fetcher} from '../client/fetcher';
 import {Toaster} from 'react-hot-toast';
 
 import 'tailwindcss/tailwind.css';
-import {io, Socket} from 'socket.io-client';
 
 export default function App({Component, pageProps}: AppProps) {
 	return (
