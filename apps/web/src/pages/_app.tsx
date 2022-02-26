4986c009c63f74e672360a1a22553201d05d6602
Alistair Smith <hi@alistair.sh>
2022-02-26 22:20:53 +0000

    bruh

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -4,13 +4,23 @@ import {SWRConfig} from 'swr';
 import {fetcher} from '../client/fetcher';
 import {Toaster} from 'react-hot-toast';
 
+import '../client/styles/global.css';
 import 'tailwindcss/tailwind.css';
 
 export default function App({Component, pageProps}: AppProps) {
 	return (
 		<SWRConfig value={{fetcher}}>
 			<Toaster />
-			<Component {...pageProps} />
+
+			<nav className="h-16 flex items-center px-8 bg-neutral-900 text-white">
+				<h2 className="text-3xl uppercase tracking-tighter font-black select-none">
+					geogig.
+				</h2>
+			</nav>
+
+			<main>
+				<Component {...pageProps} />
+			</main>
 		</SWRConfig>
 	);
 }
