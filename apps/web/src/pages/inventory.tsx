7052767d714b097c098ee0550d0c1283e1a10778
Scott Hiett <sodex.gaming@gmail.com>
2022-02-26 21:46:17 +0000

    Inventory page

--- apps/web/src/pages/inventory.tsx
+++ apps/web/src/pages/inventory.tsx
@@ -1,3 +1,58 @@
+import {InferAPIResponse} from 'nextkit';
+import {useCallback, useEffect, useState} from 'react';
+import type TicketAPI from './api/ticket';
+import {fetcher} from '../client/fetcher';
+
+type TicketResponse = InferAPIResponse<typeof TicketAPI, 'GET'>;
+
 export default function Inventory() {
-	return <h1>Your inventory of Tickets</h1>;
+	const [tickets, setTickets] = useState<TicketResponse>([]);
+
+	const revalidateTickets = useCallback(() => {
+		fetcher<TicketResponse>(`/api/ticket`)
+			.then(data => {
+				setTickets(data);
+			})
+			.catch(() => null);
+	}, []);
+
+	useEffect(() => {
+		revalidateTickets();
+	}, [revalidateTickets]);
+
+	return (
+		<div>
+			<h1>Your inventory of Tickets</h1>
+			<table className="table-auto w-full">
+				<thead>
+					<tr>
+						<th>Artist</th>
+						<th>Collection</th>
+						<th>Claim</th>
+					</tr>
+				</thead>
+				<tbody>
+					{tickets.map(ticket => (
+						<tr key={`ticket-${ticket.id}`}>
+							<td>{ticket.collection.artist.name}</td>
+							<td>{ticket.collection.name}</td>
+							<td>
+								<button
+									type="button"
+									onClick={() => {
+										// eslint-disable-next-line no-alert
+										alert(
+											"This is where you'd send us an email or something...",
+										);
+									}}
+								>
+									Claim me!
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
 }
