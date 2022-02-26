43bb7cc4948a3aa7d1d00dc6d38ee8384ca0a166
Scott Hiett <sodex.gaming@gmail.com>
2022-02-26 22:09:02 +0000

    Home screen with board of tickets

---
+++ apps/web/src/pages/api/collection/index.ts
@@ -0,0 +1,43 @@
+import {NextkitException} from 'nextkit';
+import {api} from '../../../server/api';
+import {prisma} from '../../../server/prisma';
+
+export default api({
+	async GET({context}) {
+		if (!context.userId) {
+			throw new NextkitException(401, 'You are not signed in!');
+		}
+
+		// Get the current active / future collections
+		const collections = await prisma.collection.findMany({
+			where: {
+				closes_at: {
+					gt: new Date(),
+				},
+			},
+			include: {
+				artist: true,
+				tickets: {
+					where: {
+						user_id: context.userId,
+					},
+				},
+			},
+		});
+
+		return (
+			await Promise.all(
+				collections.map(async coll => ({
+					...coll,
+					hasTicket: coll.tickets.length > 0,
+					ticketsRemaining: await prisma.ticket.count({
+						where: {
+							collection_id: coll.id,
+							user_id: null,
+						},
+					}),
+				})),
+			)
+		).filter(coll => coll.ticketsRemaining > 0);
+	},
+});
