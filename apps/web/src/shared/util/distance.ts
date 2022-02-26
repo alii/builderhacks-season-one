c09c77c77806e32da06e41ecf28096a55cecfc1c
Alistair Smith <hi@alistair.sh>
2022-02-26 22:52:09 +0000

    a

---
+++ apps/web/src/shared/util/distance.ts
@@ -0,0 +1,42 @@
+export function humanizeDistanceStringImperial(
+	meters: number,
+	abbreviate?: boolean,
+): string {
+	const feet = meters * 3.28084;
+
+	if (feet < 528) {
+		return (
+			Math.round(feet).toString() + (abbreviate === true ? ' ft' : ' feet')
+		);
+	}
+
+	return (
+		(Math.round(feet / 528) / 10).toString() +
+		(abbreviate === true ? ' mi' : ' miles')
+	);
+}
+
+export function humanizeDistanceStringMetric(meters: number): string {
+	const km = meters / 1000;
+	if (km > 100) {
+		// 100 km => 999999999 km
+		return `${km.toFixed(0)} km`;
+	}
+
+	if (km > 1) {
+		// 1.1 km => 99.9 km
+		return `${km.toFixed(1)} km`;
+	}
+
+	// 1m => 999m
+	return `${meters.toFixed(0)} m`;
+}
+
+export function humanizeDistanceString(
+	meters: number,
+	outputMetricUnits = true,
+): string {
+	return outputMetricUnits
+		? humanizeDistanceStringMetric(meters)
+		: humanizeDistanceStringImperial(meters);
+}
