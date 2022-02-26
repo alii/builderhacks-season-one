1c2876a8a2b7f94f65d800ba616a270258f188ab
Scott Hiett <sodex.gaming@gmail.com>
2022-02-26 21:22:47 +0000

    Realtime socket working

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -8,20 +8,6 @@ import 'tailwindcss/tailwind.css';
 import {io, Socket} from 'socket.io-client';
 
 export default function App({Component, pageProps}: AppProps) {
-	const socketRef = useRef<Socket>(io(`http://localhost:8081`));
-
-	useEffect(() => {
-		if (typeof window === 'undefined') {
-			return;
-		}
-
-		socketRef.current.on('request-authentication', () => {
-			socketRef.current.emit('authentication', {
-				token: window.localStorage.getItem('realtime-token'),
-			});
-		});
-	}, []);
-
 	return (
 		<SWRConfig value={{fetcher}}>
 			<Toaster />
