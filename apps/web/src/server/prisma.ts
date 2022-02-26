fa8a5dc14ecb0159b618b6cba14c953f19788c3c
Alistair Smith <hi@alistair.sh>
2022-02-26 11:43:41 +0000

    ok

--- apps/web/src/server/prisma.ts
+++ apps/web/src/server/prisma.ts
@@ -1,5 +1,11 @@
 import {PrismaClient} from '@prisma/client';
 
+// File is messy but we have to store prisma on a global variable
+// to make it survive hot reloads in Next.js
+//
+// This means that you have to restart your dev server
+// after you change the prisma schema
+
 const getClient = () => new PrismaClient();
 
 const g = global as unknown as {__prisma__: ReturnType<typeof getClient>};
