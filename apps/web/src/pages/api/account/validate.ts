07c14e32f780cd890be562c35eb3604e22352877
Scott Hiett <sodex.gaming@gmail.com>
2022-02-26 13:32:06 +0000

    Base of auth routes

---
+++ apps/web/src/pages/api/account/validate.ts
@@ -0,0 +1,46 @@
+import {api} from '../../../server/api';
+import {z} from 'zod';
+import {userSchema} from '../../../schemas/user';
+import {redis} from '../../../server/redis';
+import {NextkitException} from 'nextkit';
+import {prisma} from '../../../server/prisma';
+import {snowflake} from '../../../server/snowflake';
+
+export default api({
+	async POST({req}) {
+		const {authCode, phone} = authSchema.parse(req.body);
+
+		// Check the auth code in redis
+		const redisEntry = await redis.get(`authcode:${phone}`);
+		if (!redisEntry || redisEntry.trim() !== authCode.trim()) {
+			throw new NextkitException(422, 'Unable to find that code!');
+		}
+
+		// Find if a user already exists, and if so, return its id.
+		// If not, create a new user
+		let currentUser = await prisma.user.findFirst({
+			where: {
+				phone_number: phone,
+			},
+		});
+		if (!currentUser) {
+			currentUser = await prisma.user.create({
+				data: {
+					id: snowflake(),
+					phone_number: phone,
+				},
+			});
+		}
+
+		// TODO: Give the user a token
+		return {
+			token: 'yourmother',
+			userId: currentUser.id,
+		};
+	},
+});
+
+const authSchema = z.object({
+	phone: userSchema.phoneNumber,
+	authCode: userSchema.authCode,
+});
