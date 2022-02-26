35db7c8250667f16ffae01d5f2d71bfd418de248
Alistair Smith <hi@alistair.sh>
2022-02-26 11:48:24 +0000

    feat: fix linting

--- apps/web/src/pages/_document.tsx
+++ apps/web/src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class BuilderhacksDocument extends Document {
 					<meta name="theme-color" content="#ffffff" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
-						href="https://fonts.googleapis.com/css2?family=Krona+One&family=Roboto:wght@400;700&display=swap"
+						href="https://fonts.googleapis.com/css2?family=family=Inter:wght@400;700&display=swap"
 						rel="stylesheet"
 					/>
 				</Head>
