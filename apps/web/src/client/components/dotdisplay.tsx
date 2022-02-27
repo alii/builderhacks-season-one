67935dd60597efa7984a548e1d71999b614eea72
Scott Hiett <sodex.gaming@gmail.com>
2022-02-27 00:07:11 +0000

    Dot display

---
+++ apps/web/src/client/components/dotdisplay.tsx
@@ -0,0 +1,78 @@
+import {useEffect, useRef} from 'react';
+
+export function DotDisplay() {
+	const container = useRef<HTMLCanvasElement>(null);
+
+	useEffect(() => {
+		if (!container.current) {
+			return;
+		}
+
+		let drawing = true;
+
+		const canvas = container.current.getContext('2d')!;
+
+		let {width, height} = container.current.getBoundingClientRect();
+		container.current.width = width;
+		container.current.height = height;
+
+		const DOT_SPACING = 50;
+		const DOT_SIZE = 7;
+
+		function draw() {
+			canvas.clearRect(0, 0, width, height);
+
+			for (let x = 0; x < width; x += DOT_SPACING) {
+				for (let y = 0; y < height; y += DOT_SPACING) {
+					canvas.beginPath();
+
+					const opacity = 1 - y / height;
+
+					canvas.fillStyle = `rgba(255, 255, 255, ${opacity})`;
+
+					canvas.arc(x, y, DOT_SIZE / 2, 0, Math.PI * 2);
+					canvas.fill();
+				}
+			}
+
+			if (drawing) {
+				requestAnimationFrame(draw);
+			}
+		}
+
+		draw();
+
+		function handleResize() {
+			if (!container.current) {
+				return;
+			}
+
+			width = container.current.getBoundingClientRect().width;
+			height = container.current.getBoundingClientRect().height;
+			container.current.width = width;
+			container.current.height = height;
+		}
+
+		window.addEventListener('resize', handleResize);
+		return () => {
+			drawing = false;
+			if (container.current) {
+				window.removeEventListener('resize', handleResize);
+			}
+		};
+	}, [container]);
+
+	return (
+		<div>
+			<canvas
+				ref={container}
+				style={{
+					width: '100%',
+					opacity: '0.05',
+					height: '100%',
+					overflow: 'hidden',
+				}}
+			/>
+		</div>
+	);
+}
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,16 +1,43 @@
 import Link from 'next/link';
+import {DotDisplay} from '../client/components/dotdisplay';
 
 export default function Home() {
 	return (
-		<div>
-			<h1>welcome to geogig</h1>
-			<h2>we need a few things from you before we get you started</h2>
+		<div className="bg-zinc-800 min-h-[calc(100vh-4rem)]">
+			<div className="absolute z-0 w-full h-[calc(100vh-4rem)]">
+				<DotDisplay />
+			</div>
+			<div className="z-1 absolute w-full">
+				<div className="mx-auto max-w-7xl px-4 pt-5">
+					<h1 className="text-center text-6xl font-black uppercase text-white py-20">
+						Hunt for concert tickets in geographical regions
+					</h1>
 
-			<div className="flex space-x-2 items-center">
-				<div>
-					<Link href="/auth">
-						<a className="text-indigo-500">Continue with phone number</a>
-					</Link>
+					<div className="flex justify-center mb-14">
+						<div>
+							<Link href="/auth">
+								<a className="text-white bg-indigo-600 p-5 rounded-md">
+									Continue with phone number &rarr;
+								</a>
+							</Link>
+						</div>
+					</div>
+
+					<img
+						src={
+							'https://media.discordapp.net/attachments/947139212426244206/947275072018731048/unknown.png?width=1432&height=936'
+						}
+						alt={'example image'}
+					/>
+
+					<p className="text-center text-gray-400">
+						Made by Scott Hiett & Alistair Smith
+					</p>
+					<a href={'https://github.com/alii/builderhacks-season-one'}>
+						<p className="text-center text-indigo-400 pt-1 pb-5">
+							Read the source code on GitHub &rarr;
+						</p>
+					</a>
 				</div>
 			</div>
 		</div>
