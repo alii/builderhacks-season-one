eb320910baccd376f55ef145a27aa5be1c42ab6f
Scott Hiett <sodex.gaming@gmail.com>
2022-02-27 01:59:25 +0000

    ticket

--- apps/web/src/pages/inventory.tsx
+++ apps/web/src/pages/inventory.tsx
@@ -2,6 +2,7 @@ import {InferAPIResponse} from 'nextkit';
 import {HashLoader} from 'react-spinners';
 import useSWR from 'swr';
 import type TicketAPI from './api/ticket';
+import {Ticket} from '../client/components/ticket';
 
 type TicketResponse = InferAPIResponse<typeof TicketAPI, 'GET'>;
 
@@ -10,41 +11,31 @@ export default function Inventory() {
 
 	return (
 		<div className="mx-auto max-w-7xl px-4 py-12">
-			<h1>Your inventory of Tickets</h1>
-			<table className="table-auto w-full">
-				<thead>
-					<tr>
-						<th>Artist</th>
-						<th>Collection</th>
-						<th>Claim</th>
-					</tr>
-				</thead>
-				<tbody>
-					{tickets ? (
-						tickets.map(ticket => (
-							<tr key={`ticket-${ticket.id}`}>
-								<td>{ticket.collection.artist.name}</td>
-								<td>{ticket.collection.name}</td>
-								<td>
-									<button
-										type="button"
-										onClick={() => {
-											// eslint-disable-next-line no-alert
-											alert(
-												"This is where you'd send us an email or something...",
-											);
-										}}
-									>
-										Claim me!
-									</button>
-								</td>
-							</tr>
-						))
-					) : (
-						<HashLoader />
-					)}
-				</tbody>
-			</table>
+			<h1 className="text-2xl font-bold mb-8">Your inventory of Tickets</h1>
+
+			<div className="flex flex-row gap-5">
+				{tickets ? (
+					tickets.map(ticket => (
+						<Ticket
+							key={`ticket-${ticket.id}`}
+							artist={ticket.collection.artist.name}
+							collection={ticket.collection.name}
+							onClaimClick={() => {
+								// eslint-disable-next-line no-alert
+								alert("This is where you'd send us an email or something...");
+							}}
+						/>
+					))
+				) : (
+					<HashLoader />
+				)}
+			</div>
+
+			{tickets && (
+				<p className="text-center text-gray-500 mt-10">
+					That's all! Keep hunting for tickets to build up your library!
+				</p>
+			)}
 		</div>
 	);
 }
